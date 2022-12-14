9ac690c83b5bc197920f2e25aedd350e3e5fe9d4
Moacir Amaro <moacirjgt@gmail.com>
2022-12-13 21:00:48 -0300

    add new methods to classes

--- ex01/includes/Contact.hpp
+++ ex01/includes/Contact.hpp
@@ -1,8 +1,7 @@
-#ifndef CONTACT_H
-#define CONTACT_H
+#ifndef CONTACT_HPP
+#define CONTACT_HPP
 #include<iostream>
 #include <string>
-
 class Contact
 {
 private:
@@ -12,18 +11,81 @@ private:
 	std::string phoneNumber;
 	std::string darkestSecret;
 public:
-	Contact(/* args */);
+	Contact() {}
+	Contact(std::string firstName, std::string lastName, std::string nickname, std::string phoneNumber, std::string darkestSecret) {
+		setFirstName(firstName);
+		setLastName(lastName);
+		setNickname(nickname);
+		setPhoneNumber(phoneNumber);
+		setDarkestSecret(darkestSecret);
+	}
 	~Contact();
+
+	void setFirstName(std::string firstName) {
+		if (firstName.empty()) {
+			std::cout << "First name cannot be empty" << std::endl;
+		}
+		else {
+			this->firstName = firstName;
+		}
+	}
+
+	void setLastName(std::string lastName) {
+		if (lastName.empty()) {
+			std::cout << "Last name cannot be empty" << std::endl;
+		}
+		else {
+			this->lastName = lastName;
+		}
+	}
+
+	void setNickname(std::string nickname) {
+		if (nickname.empty()) {
+			std::cout << "Nickname cannot be empty" << std::endl;
+		}
+		else {
+			this->nickname = nickname;
+		}
+	}
+
+	void setPhoneNumber(std::string phoneNumber) {
+		if (phoneNumber.empty()) {
+			std::cout << "Phone number cannot be empty" << std::endl;
+		}
+		else {
+			this->phoneNumber = phoneNumber;
+		}
+	}
+
+	void setDarkestSecret(std::string darkestSecret) {
+		if (darkestSecret.empty()) {
+			std::cout << "Darkest secret cannot be empty" << std::endl;
+		}
+		else {
+			this->darkestSecret = darkestSecret;
+		}
+	}
+
+	std::string getFirstName() {
+		return this->firstName;
+	}
+
+	std::string getLastName() {
+		return this->lastName;
+	}
+
+	std::string getNickname() {
+		return this->nickname;
+	}
+
+	std::string getPhoneNumber() {
+		return this->phoneNumber;
+	}
+
+	std::string getDarkestSecret() {
+		return this->darkestSecret;
+	}
+	
 };
-
-Contact::Contact(/* args */)
-{
-}
-
-Contact::~Contact()
-{
-}
-
-
 
 #endif
--- ex01/includes/PhoneBook.hpp
+++ ex01/includes/PhoneBook.hpp
@@ -1,5 +1,56 @@
-#ifndef PHONEBOOK_H
-#define PHONEBOOK_H
-#include<iostream> 
+# ifndef PHONEBOOK_HPP
+# define PHONEBOOK_HPP
+# include<iostream>
+# include "Contact.hpp"
 
-#endif
+class PhoneBook {
+private:
+	Contact contacts[8];
+	int contactCount;
+public:
+	PhoneBook(){}
+	PhoneBook() {
+		this->contactCount = 0;
+	}
+
+	~PhoneBook();
+
+	void addNewContact(Contact contact) {
+		if(contactCount < 8) {
+			this->contacts[this->contactCount] = contact;
+			return ;
+		}
+		std::cout << 'PhoneBook is Already full' << '\n';
+	}
+
+	std::string parseInfo(std::string info) {
+		std::string parsedInfo;
+
+		parsedInfo = info.substr(0, 10);
+	}
+
+	void showContacts() {
+		Contact currenctContact;
+		std::cout << "index\t|\tfirst name\t|\tlast name\t|\tnickname" << '\n';
+		for(int index = 0; index < this->contactCount; index++) {
+			currenctContact = this->contacts[index];
+			std::cout << "[ " << index << " ]";
+			std::cout << this->parseInfo(currenctContact.getFirstName());
+			std::cout << this->parseInfo(currenctContact.getLastName());
+			std::cout << this->parseInfo(currenctContact.getNickname());
+		}
+	};
+	void showContact(int index) {
+
+	};
+
+	void searchContacts(int index) {
+		if(index < 1 || index > 8) {
+			std::cout << 'Invalid index' << '\n';
+			return ;
+		}
+		this->showContacts();
+		this->showContact(index);
+	}
+}
+# endif
